5dcc69a0b1fb0e1739004d9f612abd7314d7251f
ThaiLe1220 <lehongthai2000@gmail.com>
2024-01-24 12:47:16 +0700

    improve extract html for more consistent translation

--- eugene-netflix-selenium/my-app/extract_html.py
+++ eugene-netflix-selenium/my-app/extract_html.py
@@ -4,14 +4,29 @@ import re
 import os
 
 
-def clean_text(text):
-    target = text.replace("\n                    ", " ").strip()
+def clean_translation(text):
+    target = text.replace("\n                    ", "").strip()
     target = target.replace("\n", " ")
     target = target.replace("\u200E", "")
     target = target.replace('"', "")
-
-    # Replace both "[]" and the text inside it [....text...] with empty string
     target = re.sub(r"\[.*?\]", "", target)
+
+    return target
+
+
+import re
+
+
+def clean_subtitle(text):
+    # Replace newline and unwanted characters
+    target = text.replace("\n", " ")
+    target = target.replace("\u200E", "")
+    target = target.replace('"', "")
+    target = re.sub(r"\[.*?\]", "", target)
+
+    target = re.sub(r"-(?![\s-])", "- ", target)
+
+    target = target.strip()
 
     return target
 
@@ -21,10 +36,10 @@ def process_subtitle(td):
     for element in td.children:
         if element.name == "span":
             text = element.get_text()
-            text = text.replace("\n", " ")
             processed_subtitle += text + ""
 
-    return processed_subtitle.strip()
+    processed_subtitle = clean_subtitle(processed_subtitle)
+    return processed_subtitle
 
 
 def process_and_save_data(movies_id, lang_code):
@@ -43,7 +58,7 @@ def process_and_save_data(movies_id, lang_code):
             subtitle = process_subtitle(cols[1])
 
             if len(cols) >= 3:
-                translation = clean_text(cols[2].get_text())
+                translation = clean_translation(cols[2].get_text())
             else:
                 translation = ""
 
@@ -87,5 +102,5 @@ with open("./source/movies_links_cleaned.txt", "r") as file:
 
 
 for i, link in enumerate(netflix_links):
-    if 0 <= i <= 100:
-        process_and_save_data(movies_ids[i], "fr")
+    if 1090 <= i <= 1095:
+        process_and_save_data(movies_ids[i], "vi")
